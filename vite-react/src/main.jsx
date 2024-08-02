8ed41a314bf230385f8006441fe3a879d9a543c3
heejae <leeheejaelcode@gmail.com>
2024-08-02 18:50:28 +0900

    [환경구성] vite-react 속성

---
+++ vite-react/src/main.jsx
@@ -0,0 +1,11 @@
+import '@/styles/main.css';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('react-app');
+
+if (container) {
+  createRoot(container).render(<StrictMode></StrictMode>);
+} else {
+  console.warn('문서에 "#app" 요소가 존재하지 않습니다.');
+}
